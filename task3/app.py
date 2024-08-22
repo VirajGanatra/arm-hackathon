92200afe63f83e799ca797c4f990251b66e64ef8
Viraj Ganatra <viraj.ganatra@arm.com>
2024-08-22 11:54:49 +0100

    fix: disable weekends

--- task3/app.py
+++ task3/app.py
@@ -14,11 +14,16 @@ def calculate():
     stock = request.form['stock']
     amount_spent = request.form['amount_spent']
     
+    
+    start_date_obj = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    
+    
+    if start_date_obj.weekday() >= 5:
+        return render_template('index.html', error="Start date cannot be a weekend. Please select a weekday.")
+    
     price_diff = StockPrice(stock, amount_spent, start_date).calculate_price_difference(start_date)
     
     return render_template('result.html', price_diff=price_diff)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
