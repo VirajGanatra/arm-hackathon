dd6cb5e3bd126e5bf7be8b48fcd11f2501702318
Joshua Jose <56792649+joshua-jose@users.noreply.github.com>
2024-08-22 11:40:35 +0100

    Building arbiter

--- hardware/arbiter.v
+++ hardware/arbiter.v
@@ -1,32 +1,54 @@
 `timescale 1ns / 1ps
 
 module arbiter (
-    input  r[2:0],
-    output g[2:0]
+    input  clk,
+    rst,
+    input  r  [2:0],
+    output g  [2:0]
 );
   parameter A = 2'b00,  // "A" State
   B = 2'b01,  // "B" State
   C = 2'b10,  // "C" State
   D = 2'b11;  // "D" State
 
-  reg [1:0] state = A;
+  reg [1:0] current_state, next_state;
 
-  always @* begin
-    if (state == A && r[0]) begin
-      state = B;
-    end else if (state == A && r[1]) begin
-      state = C;
-    end else if (state == A && r[2]) begin
-      state = D;
-
-    end else if (state == B && ~r[0]) begin
-      state = A;
-    end else if (state == C && ~r[1]) begin
-      state = A;
-    end else if (state == D && ~r[2]) begin
-      state = A;
-    end
-
+  always @(posedge clk, posedge rst) begin
+    if (rst) current_state <= A;
+    else current_state <= next_state;
   end
+
+  always_latch @* begin
+    case (current_state)
+      A: begin
+        if (r[0]) begin
+          next_state = B;
+        end else if (r[1]) begin
+          next_state = C;
+        end else if (r[2]) begin
+          next_state = D;
+        end
+      end
+      B: begin
+        if (~r[0]) begin
+          next_state = A;
+        end
+      end
+      C: begin
+        if (~r[1]) begin
+          next_state = A;
+        end
+      end
+      D: begin
+        if (~r[2]) begin
+          next_state = A;
+        end
+      end
+    endcase
+  end
+
+  assign g[0] = (current_state == B);
+  assign g[1] = (current_state == C);
+  assign g[2] = (current_state == D);
 
 endmodule
